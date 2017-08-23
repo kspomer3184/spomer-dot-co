cd767d58a6b5d31340bc4c47ccaf0ef76242d3d2
Kyle Spomer <kspomer3184@flpoly.org>
2017-08-23 11:54:21 -0400

    reinitialized

---
+++ pages/sandbox/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>PHP FTW!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+            <?php
+                $myName = "Kyle";
+                $myAge = 19;
+                echo $myName;
+                echo $myAge;
+            ?>
+        </p>
+	</body>
+</html>
