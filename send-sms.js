2c941bb69fc077541187d0cdf0bd2edb258926aa
Kyle Spomer <kspomer3184@flpoly.org>
2017-11-20 14:03:12 +0000

    Ignore this commit

--- send-sms.js
+++ send-sms.js
@@ -1,14 +1,6 @@
-let message = prompt("What Message would you like to send?","");
+let first = prompt("First Name:", "");
+let last = promt("Last Name:", "");
 
-// Twilio Credentials 
-const accountSid = 'AC24b3c0ad866586038e4f5ca818ae30ed';
-const authToken = '3bab3d6d5bef27855cfb5bada3c39346'; 
-//require the Twilio module and create a REST client 
-const client = require('twilio')(accountSid, authToken); 
-client.messages
-  .create({
-    to: '+14077583284',
-    from: '+14079019282',
-    body: message,
-  })
-  .then((message) => console.log(message.sid));
+message = first + last;
+
+alert(message);
